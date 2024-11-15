580bbfed4c525715fb0bafe7d6c056484cc8ea9c
Julia Alvarado Rubio <juliaalvaradorubio@gmail.com>
2024-11-15 13:10:14 +0000

    Implement MOT.cs for fetching vehicle details from the MOT API

---
+++ BlazorApp1/BlazorApp1/Services/MOT.cs
@@ -0,0 +1,52 @@
+//importing whats needed
+
+using System.Net.Http.Headers; 
+using System.Text.Json;
+using System.Threading.Tasks; //asynchronous methods
+
+namespace BupaMOTApp.Services
+{
+    public class MOTService
+    {
+        private readonly HttpClient _httpClient;
+
+        public MOTService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<VehicleMOT> GetMOTDetailsAsync(string registration)
+        {
+            // The base URL for the MOT API endpoint
+            var url = $"https://beta.check-mot.service.gov.uk/trade/vehicles/mot-tests?registration={registration}";
+
+            _httpClient.DefaultRequestHeaders.Clear();
+            _httpClient.DefaultRequestHeaders.Add("x-api-key", "fZi8YcjrZN1cGkQeZP7Uaa4rTxua8HovaswPuIno"); //this is the API key we need to be able to retrieve the information
+
+            // GET request 
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                // convert into object
+                var content = await response.Content.ReadAsStringAsync();
+                var vehicleMOT = JsonSerializer.Deserialize<VehicleMOT>(content);
+                return vehicleMOT;
+            }
+
+            // If the response is not successful, return null
+            return null;
+        }
+    }
+
+    public class VehicleMOT
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Colour { get; set; }
+        public string MOTExpiryDate { get; set; }
+        public int MileageAtLastMOT { get; set; }
+    }
+}
+
+
--- BlazorApp1/BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d4cdaa8b1f903fbbabf49ef48fa3c12ccc57e48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c2f3bd5f23bf6b3aca8e7404b3179515b9a61a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
